9a7f579258d1bb690d32513bf8c4b469e07ff6db
lavyun <lavyun@163.com>
2017-02-22 11:26:48 +0800

    优化

---
+++ src/components/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+      <panel></panel>
+  </div>
+</template>
+
+<script>
+import panel from './components/search-panel.vue';
+
+
+export default {
+  components: {
+    panel
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+
+  height: 100%;
+  background:url('./assets/background.png') no-repeat;background-size: 100% 100%
+}
+
+#app {
+  color: #2c3e50;
+  width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+  position: absolute;
+  top: 35%;
+  left: 50%;
+  margin-left: -300px;
+  margin-top: -100px;
+}
+
+</style>
+
